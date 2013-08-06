89a807dcdb9192ee507d18441022cbc214b82c72
Rob Larsen <rob@htmlcssjavascript.com>
2013-08-06 12:10:33 -0400

    Improved interface
    plus a simple i/o script

--- data/index.php
+++ data/index.php
@@ -1,10 +1,12 @@
 <?php
-	$data = 'data.json';
+	$data = 'books.json';
 	if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     header('Content-Type: text/javascript');
 		readfile("books.json");
 	}	
 	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // …
+     $fh = fopen($data, 'w');
+    fwrite($fh, '{"books":'.$HTTP_RAW_POST_DATA.'}');
+    fclose($fh);
 	}	
 ?>

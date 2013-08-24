cd04c69b6bab699d4bea2de4937d64ffbcceb811
Rob Larsen <rob@htmlcssjavascript.com>
2013-08-24 08:27:42 -0400

    data updates and reorg for beta

--- js/controllers.js
+++ js/controllers.js
@@ -1,11 +1,74 @@
-'use strict';
+angular.module('comicsApp.controllers', ['comicFilters']).
+  controller('comicsCtrl', ["$scope", "$http",
+    function( $scope , $http)  {
+      $http({"method" : "GET", "url" : "data/books.json"}).success(
+        function(data){
+         $scope.items = data.books;
+         var titles = [], pedigrees = [], publishers = [], venues = [];
+         for (var i=0; i < $scope.items.length; i++){
+            if (!_.contains(titles,$scope.items[i].title)){
+              titles.push($scope.items[i].title)
+            }
+            if ($scope.items[i].pedigree){
+              if (!_.contains(pedigrees,$scope.items[i].pedigree)){
+                pedigrees.push($scope.items[i].pedigree)
+              }         
+            }
+            if ($scope.items[i].publisher){
+              if (!_.contains(publishers,$scope.items[i].publisher)){
+                publishers.push($scope.items[i].publisher)
+              }         
+            }
+            if ($scope.items[i].sales.length){
+              for (var j=0; j < $scope.items[i].sales.length; j++){
 
-/* Controllers */
-
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
+                if (!_.contains(venues,$scope.items[i].sales[j].venue)){
+                  venues.push($scope.items[i].sales[j].venue)
+                }       
+          
+              }  
+            }  
+         }
+        $scope.titles = titles;
+        $scope.publishers = publishers;
+        $scope.pedigrees = pedigrees; 
+        $scope.venues = venues; 
+        $scope.uid = data.books.length;
+      });
+      $scope.sales = [];
+      $scope.addItem = function(item) { 
+        item.sales = $scope.sales;
+        item.uid = $scope.uid;
+        $scope.uid++;
+        $scope.items.push(item);
+        $scope.item = {};
+        $scope.sales = [];
+        if (item.pedigree){
+          if (!_.contains($scope.pedigrees,item.pedigree)){
+            $scope.pedigrees.push(item.pedigree)
+          }         
+        }
+        if (item.publisher){
+          if (!_.contains($scope.publishers,item.publisher)){
+            $scope.publishers.push(item.publisher)
+          }         
+        }
+        if (!_.contains($scope.titles,item.title)){
+          $scope.titles.push(item.title)
+        }
+        var items = $scope.items,
+          len = items.length;
+        for ( var i = 0; i <len; i++){
+          delete items[i]["$$hashKey"];
+        }
+        $http.post('data/index.php', JSON.stringify(items)).success(function(data){console.log("200");
+        }); 
+      } 
+      $scope.addSale = function(sale) {
+        $scope.sales.push(_.clone(sale));
+        for (var val in sale){
+          delete sale[val];
+        }
+      } 
+  }
+])
--- js/filters.js
+++ js/filters.js
@@ -1,10 +1,15 @@
-'use strict';
-
-/* Filters */
-
-angular.module('myApp.filters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
+angular.module('comicFilters', []).filter('srcFilter', function() {
+  return function(input) {
+    if (input === 'cgc' || input === 'pgx' ) {
+      input = input.toUpperCase();
     }
-  }]);
+    return input;
+  };
+}).filter('capitalize', function () {
+    "use strict";
+    return function (input) {
+      if (input){
+        return input.charAt(0).toUpperCase() + input.slice(1);
+      }
+    };
+});

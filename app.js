b7ded6f3a28d45e495fb033200f764a347528a08
Rob Larsen <rob@htmlcssjavascript.com>
2017-01-19 19:57:44 -0500

    amazing fantasy 15 cgc 9.2

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 var express = require('express');
 var app = express();
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
 
+// Connection URL
+var url = 'mongodb://localhost:27017/comics';
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+});
+var findDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs)
+    callback(docs);
+  });
+}
+findDocuments();
 app.use(express.static('static'));
 
 app.get('/data/books.json', function (req, res, next) {
@@ -51,5 +73,5 @@ app.get('/data/sa-pedigrees.json', function (req, res, next) {
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('$100k club app listening on port 3000!');
 })

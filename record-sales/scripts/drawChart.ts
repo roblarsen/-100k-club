81023b8cca35fc87679045fa4d8cb152c9f8adb4
Rob Larsen <rob@htmlcssjavascript.com>
2025-05-09 20:40:59 +0000

    colors!

--- record-sales/scripts/drawChart.ts
+++ record-sales/scripts/drawChart.ts
@@ -4,9 +4,19 @@ import * as d3 from "d3";
 
 export function drawChart(data: Array<RecordSale>) {
 
+  const colors = {
+    "comicconnect":"#3EF77F",
+"heritage":"#F7AF3E",
+"comiclink":"#3EF7E8",
+"pedigreecomics":"#F77A3E",
+"metropolis":"#5FA29D",
+"default":"#5A7864"
+  };
+
    const years = [];
     data.forEach((d: RecordSale) => {
       const mdy = d.salesDate.split("-");
+      console.log(Number(mdy[0]));
       if (Number(mdy[0])> 1901) {
         years.push(mdy[0]);
       }
@@ -28,8 +38,6 @@ let svg = d3.select("#viz")
   .append("g")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
-
-          console.log(maxYear,minYear);
 
   // Add X axis
  const x = d3.scaleTime(
@@ -59,63 +67,28 @@ let svg = d3.select("#viz")
  
  
   // Add Y axis
-  const y = d3.scaleLog()
-    .domain([100000, maxPrice])
+  const y = d3.scaleLinear()
+    .domain([90000, maxPrice+(maxPrice*.1)])
     .range([ height, 0]);
   svg.append("g")
     .call(d3.axisLeft(y));
 
-  // Add a tooltip div. Here I define the general feature of the tooltip: stuff that do not depend on the data point.
-  // Its opacity is set to 0: we don't see it by default.
-  var tooltip = d3.select("#my_dataviz")
-    .append("div")
-    .style("opacity", 0)
-    .attr("class", "tooltip")
-    .style("background-color", "white")
-    .style("border", "solid")
-    .style("border-width", "1px")
-    .style("border-radius", "5px")
-    .style("padding", "10px")
-
-
-
-  // A function that change this tooltip when the user hover a point.
-  // Its opacity is set to 1: we can now see it. Plus it set the text and position of tooltip depending on the datapoint (d)
-  var mouseover = function(d) {
-    tooltip
-      .style("opacity", 1)
-  }
-
-  var mousemove = function(d) {
-    tooltip
-      .html("The exact value of<br>the Ground Living area is: " + d.GrLivArea)
-      .style("left", (d3.mouse(this)[0]+90) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
-      .style("top", (d3.mouse(this)[1]) + "px")
-  }
-
-  // A function that change this tooltip when the leaves a point: just need to set opacity to 0 again
-  var mouseleave = function(d) {
-    tooltip
-      .transition()
-      .duration(200)
-      .style("opacity", 0)
-  }
-
-  // Add dots
-  svg.append('g')
+    svg.append('g')
     .selectAll("dot")
     .data(data) 
     .enter()
     .append("circle")
-      .attr("cx", function (d) { return x(new Date(d.salesDate)); } )
-      .attr("cy", function (d) { return y(d.price); } )
+      .attr("cx", function (d:RecordSale) { return x(new Date(d.salesDate)); } )
+      .attr("cy", function (d:RecordSale) { return y(d.price); } )
       .attr("r", 7)
-      .style("fill", "#69b3a2")
+      .style("fill", function (d:RecordSale) { 
+        if (d.venue.toLocaleLowerCase() in colors) {
+          return colors[d.venue.toLocaleLowerCase()];
+        } else {
+          return colors["default"];
+        }
+       })
       .style("opacity", 0.3)
-      .style("stroke", "white")
-    .on("mouseover", mouseover )
-    .on("mousemove", mousemove )
-    .on("mouseleave", mouseleave )
 
 
     

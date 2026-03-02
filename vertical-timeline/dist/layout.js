2fc6a49d9ed2538c3ccb96d1231b30bbf69f5e6b
Rob Larsen <rob@htmlcssjavascript.com>
2026-03-02 01:08:45 +0000

    typescript

---
+++ vertical-timeline/dist/layout.js
@@ -0,0 +1,21 @@
+export function processTimelineData(rawData, config) {
+    let globalLastY = config.minStartY;
+    const processedData = rawData.map((entry, index) => {
+        const isLeft = index % 2 === 0;
+        const date = new Date(entry.date);
+        let targetY = (date.getFullYear() - config.startYear + (date.getMonth() / 12)) * config.pixelsPerYear + config.initialOffsetY;
+        if (targetY < globalLastY + config.collisionSpacing) {
+            targetY = globalLastY + config.collisionSpacing + config.boxH / 2;
+        }
+        globalLastY = targetY + config.boxH / 2;
+        return {
+            ...entry,
+            y: targetY,
+            side: isLeft ? "left" : "right"
+        };
+    });
+    return {
+        processedData,
+        totalHeight: globalLastY + config.bottomPadding
+    };
+}
---
+++ vertical-timeline/dist/main.js
@@ -0,0 +1,11 @@
+import { rawData } from "./data";
+import { CHART_CONFIG } from "./config";
+import { processTimelineData } from "./layout";
+import { renderTimeline } from "./render";
+const { processedData, totalHeight } = processTimelineData(rawData, CHART_CONFIG);
+renderTimeline({
+    containerSelector: "#chart-container",
+    processedData,
+    totalHeight,
+    config: CHART_CONFIG
+});
